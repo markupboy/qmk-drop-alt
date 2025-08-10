0599ab1cc3cc1f58e8eb4447a2bba3adb75bf3d4
Blake Walters <blake@markupboy.com>
2025-08-10 10:32:54 -0600

    left control is now hold for lctl double tap for caps lock

--- keymap.c
+++ keymap.c
@@ -1,5 +1,10 @@
 #include QMK_KEYBOARD_H
 
+// Tap dance declarations
+enum tap_dance_codes {
+    CTRL_CAPS = 0
+};
+
 // Custom keycodes for theme switching, hue, brightness, and saturation adjustment
 enum custom_keycodes {
     THEME_OUTRUN = SAFE_RANGE,
@@ -23,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
         QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGUP,
+        TD(CTRL_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGUP,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN,
         MO(2), KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
     
@@ -37,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [2] = LAYOUT_65_ansi_blocker(
       _______, _______, _______, _______ , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-      _______, _______, KC_UP  , _______ , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+      _______, KC_HOME, KC_UP  , KC_END  , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
       _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
       _______, _______, _______, _______ , _______, _______, _______, _______, _______, _______, _______, _______,          KC_PGUP, _______, \
       _______, _______, _______,                             _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END  \
@@ -118,3 +123,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true; // Process all other keycodes normally
 }
+
+// Tap dance functions
+void dance_ctrl_caps_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        register_code(KC_LCTL);
+    } else if (state->count == 2) {
+        tap_code(KC_CAPS);
+    }
+}
+
+void dance_ctrl_caps_reset(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        unregister_code(KC_LCTL);
+    }
+}
+
+// Tap dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    [CTRL_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_ctrl_caps_finished, dance_ctrl_caps_reset)
+};
